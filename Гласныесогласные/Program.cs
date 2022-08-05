12db0c85f20bcc62c020af8a4dfa7144eb8ca109
Alyona Karpenko <88236583+floweroom@users.noreply.github.com>
2022-08-05 19:52:09 +1000

    Commit

---
+++ Гласныесогласные/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Гласныесогласные
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+         
+            
+
+            string word = "сенсация";
+          
+            string sign = "аеёиоуыэюя";
+            for (int ch = 0; ch < sign.Length; ch++) 
+            {
+                if(word.Contains(sign[ch]))
+                    count++;
+            } 
+           
+            Console.WriteLine(count);
+            Console.ReadLine();
+          
+              
+        }
+    }
+}
