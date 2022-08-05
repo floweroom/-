39202f6d759e7f6f7cfd9f16b3a46006be269554
Alyona Karpenko <88236583+floweroom@users.noreply.github.com>
2022-08-05 20:10:59 +1000

    Second Commit

--- Гласныесогласные/Program.cs
+++ Гласныесогласные/Program.cs
@@ -8,19 +8,28 @@ namespace Гласныесогласные
         static void Main(string[] args)
         {
             int count = 0;
-         
-            
 
-            string word = "сенсация";
+            int count2 = 0;
+
+            string word1 = "Сенация";
+           string word =word1.ToLower();
           
             string sign = "аеёиоуыэюя";
             for (int ch = 0; ch < sign.Length; ch++) 
             {
                 if(word.Contains(sign[ch]))
                     count++;
-            } 
-           
-            Console.WriteLine(count);
+            }
+            string sign2 = "бвгджзйклмнпрстфхцчшщ";
+            for (int ch = 0; ch < sign2.Length; ch++)
+            {
+                if (word.Contains(sign2[ch]))
+                    count2++;
+            }
+
+
+            Console.WriteLine($"Количество гласных букв{ count}");
+            Console.WriteLine($"Количество согласных букв{count2}");
             Console.ReadLine();
           
               
